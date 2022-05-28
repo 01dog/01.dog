c4491c441d2f7d238c6163e47b5b2284155a02ef
01dog <bellwitch@pm.me>
2022-05-27 20:02:40 -0400

    some social buttons set up, added copy function for discord tag

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,11 +1,11 @@
 <div class="navbar mb-5 shadow-lg bg-neutral text-neutral-content">
-    <div class="flex-none px-2 mx-2">
-        <span class="text-lg font-bold text-primary">1dog</span>
-    </div>
-    <div class="flex-1 px-2 mx-2">
-        <div class="text-base-content items-stretch hidden lg:flex">
-            <a href="/" class="btn btn-ghost btn-sm rounded-btn">home</a>
-            <!-- <a href="/blog" class="btn btn-ghost btn-sm rounded-btn">blog</a> -->
-        </div>
-    </div>
+	<div class="flex-none px-2 mx-2">
+		<span class="text-lg font-bold text-primary">1dog</span>
+	</div>
+	<div class="flex-1 px-2 mx-2">
+		<div class="text-base-content items-stretch hidden lg:flex">
+			<a href="/" class="btn btn-ghost btn-sm rounded-btn">home</a>
+			<a href="/blog" class="btn btn-ghost btn-sm rounded-btn">blog</a>
+		</div>
+	</div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,31 +1,33 @@
 <script context="module">
-    import Hero from '$lib/components/Hero.svelte'
-    import Socials from '$lib/components/Socials.svelte'
+	import Hero from '$lib/components/Hero.svelte';
+	import Socials from '$lib/components/Socials.svelte';
 
-    import { getAboutMe } from '$lib/queries/graphQLQueries'
-    import { client } from '$lib/utils/graphQLClient'
+	import { getAboutMe } from '$lib/queries/graphQLQueries';
+	import { client } from '$lib/utils/graphQLClient';
 
-    export const load = async () => {
-        const { author } = await client.request(getAboutMe)
-        return {
-            props: {
-                author,
-            },
-        }
-    }
+	export const load = async () => {
+		const { author } = await client.request(getAboutMe);
+		return {
+			props: {
+				author
+			}
+		};
+	};
 </script>
 
 <!--this feels incorrect somehow-->
 <script>
-    export let author
+	export let author;
 </script>
 
 <svelte:head>
-    <title>01.dog</title>
+	<title>1dog</title>
 </svelte:head>
 
 <div>
-    <Hero {author}/>
+	<Hero {author} />
 </div>
-<h2 class="text-4xl font-bold text-center">test </h2>
-<Socials />
+
+<div class="grid justify-center mb-6">
+	<Socials />
+</div>
