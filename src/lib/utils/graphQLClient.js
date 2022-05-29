6f24fe54603f7b1aa5401f090cfa450cbf544135
01dog <bellwitch@pm.me>
2022-05-29 14:35:09 -0400

    update deps, test to fix netlify 500 error

--- src/lib/utils/graphQLClient.js
+++ src/lib/utils/graphQLClient.js
@@ -1,4 +1,4 @@
 import { GraphQLClient } from 'graphql-request'
-const GRAPHCMS_ENDPOINT = import.meta.env.VITE_GRAPHCMS_URL
+// const GRAPHCMS_ENDPOINT = import.meta.env.VITE_GRAPHCMS_URL
 
-export const client = new GraphQLClient(GRAPHCMS_ENDPOINT)
+export const client = new GraphQLClient("https://api-us-east-1.graphcms.com/v2/cl3hqqss0669z01xphutchbw8/master")
