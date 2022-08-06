0b3292036790d76c5009abff8a2e3f3bbb522283
01dog <bellwitch@pm.me>
2022-08-05 21:28:51 -0400

    look for valid survey URL in cors-every response redid response logic

--- api/rfn.js
+++ api/rfn.js
@@ -1,4 +1,4 @@
-import { AbortError } from 'p-retry';
+// import { AbortError } from 'p-retry';
 import fetch from 'isomorphic-fetch';
 
 export default function buildSurveyURL(data) {
@@ -11,7 +11,7 @@ export default function buildSurveyURL(data) {
 }
 
 // probably a better way to do this
-let randomNum = 0; // 0
+let randomNum = 0;
 async function findRandomNum(surveyNum, keywordNum) {
     let baseURL = "https://shielded-beach-28637.herokuapp.com/https://feedback.inmoment.com/websurvey/2/begin";
         
@@ -20,17 +20,34 @@ async function findRandomNum(surveyNum, keywordNum) {
     let newURL = baseURL + '?gateway=WalgreensQR' + '&survey=' + surveyNum + '&keyword=' + keywordNum + "&timeofvisit=1200";
 
     const response = await fetch(newURL);
+    let foundGoodResponse = checkFinalURL(response)
 
-    if (response.status === 303) {
-        console.log("got the secret number! it is ", randomNum);
-        
-        randomNum = 0;
-        return surveyNum + randomNum + keywordNum;
-    } else if (response.status === 404) {
-        throw new AbortError(response.statusText);
+    if (!foundGoodResponse && randomNum <10) {
+        console.log(randomNum)
+        randomNum++
+        findRandomNum(surveyNum, keywordNum)
+    } else if (foundGoodResponse) {
+        console.log("we found a valid RFN!")
+        return surveyNum + randomNum + keywordNum
     }
-    randomNum++
+    // if (response.status === 303) {
+    //     console.log("got the secret number! it is ", randomNum);
+        
+    //     return surveyNum + randomNum + keywordNum;
+    // } else if (response.status === 404) {
+    //     throw new AbortError(response.statusText);
+    // }
 
     // console.log(await pRetry(findRandomNum, {retries: 10}))
-    console.log("did not get valid RFN")
+    console.log("did not get valid RFN after 10 loops")
+    randomNum = 0;
+}
+
+function checkFinalURL(response) {
+    let finalURL = response.headers.get('X-Final-Url');
+    if (finalURL.includes("execute")) {
+        console.log("we found a valid survey URL")
+        return true;
+    }
+    return false;
 }
