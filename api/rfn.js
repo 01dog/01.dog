6417880a833c9145792044d9dd7344adc9baffca
01dog <bellwitch@pm.me>
2022-08-05 21:57:47 -0400

    another big RFN logic rewrite, now much cleaner

--- api/rfn.js
+++ api/rfn.js
@@ -1,53 +1,26 @@
 // import { AbortError } from 'p-retry';
 import fetch from 'isomorphic-fetch';
 
-export default function buildSurveyURL(data) {
+export default async function buildSurveyURL(data) {
     let survey, keyword;
+    let baseURL = "https://shielded-beach-28637.herokuapp.com/https://feedback.inmoment.com/websurvey/2/begin";
     
     survey = data.storenum + data.termnum + data.transid;
     keyword = data.year + data.month + data.day + data.secret
 
-    return findRandomNum(survey, keyword)
-}
+    for (let randomNum = 0; randomNum < 9; randomNum++) {
+        let fullURL = baseURL + '?gateway=WalgreensQR&survey=' + survey + '&keyword=' + randomNum + keyword + "&timeofvisit=1200";
+        console.log("BUILT URL: ", fullURL);
+        let currentRFN = survey + randomNum + keyword;
+        console.log("CURRENT RFN IS: ", currentRFN);
 
-// probably a better way to do this
-let randomNum = 0;
-async function findRandomNum(surveyNum, keywordNum) {
-    let baseURL = "https://shielded-beach-28637.herokuapp.com/https://feedback.inmoment.com/websurvey/2/begin";
-        
-    keywordNum = randomNum + keywordNum;
+        const response = await fetch(fullURL);
 
-    let newURL = baseURL + '?gateway=WalgreensQR' + '&survey=' + surveyNum + '&keyword=' + keywordNum + "&timeofvisit=1200";
-
-    const response = await fetch(newURL);
-    let foundGoodResponse = checkFinalURL(response)
-
-    if (!foundGoodResponse && randomNum <10) {
-        console.log(randomNum)
-        randomNum++
-        findRandomNum(surveyNum, keywordNum)
-    } else if (foundGoodResponse) {
-        console.log("we found a valid RFN!")
-        return surveyNum + randomNum + keywordNum
+        let finalURL = response.headers.get('X-Final-Url');
+        if (finalURL.includes("execute")) {
+            console.log("WE GOT A VALID RFN!")
+            return survey + randomNum + keyword
+        }
+        console.log("we didn't get a valid RFN")
     }
-    // if (response.status === 303) {
-    //     console.log("got the secret number! it is ", randomNum);
-        
-    //     return surveyNum + randomNum + keywordNum;
-    // } else if (response.status === 404) {
-    //     throw new AbortError(response.statusText);
-    // }
-
-    // console.log(await pRetry(findRandomNum, {retries: 10}))
-    console.log("did not get valid RFN after 10 loops")
-    randomNum = 0;
-}
-
-function checkFinalURL(response) {
-    let finalURL = response.headers.get('X-Final-Url');
-    if (finalURL.includes("execute")) {
-        console.log("we found a valid survey URL")
-        return true;
-    }
-    return false;
 }
