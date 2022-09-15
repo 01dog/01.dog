4c5189ad79df503c21c78c53829561cc3ca87fd3
01dog <bellwitch@pm.me>
2022-09-15 12:44:16 -0400

    final cleanup

--- middleware.js
+++ middleware.js
@@ -10,6 +10,11 @@ export const config = {
 export default function middleware(request) {
     const auth = request.headers.get("Authorization")
 
+    /* TODO: btoa should not be used here and should be replaced with something like
+    Buffer.from(str, 'base64') and buf.toString('base64). however...
+    Buffer.from() isn't encoding the env variable correctly, and i dont know why
+    for now, i'll leave it since this (hopefully) won't always need auth, but this should
+    be addressed at some point.**/
     if (auth !== `Basic ${btoa(process.env.RFN_LOGIN)}`) {
         return new Response("Not authorized", {
             status: 401,
@@ -20,5 +25,5 @@ export default function middleware(request) {
         })
     }
     
-    return next();
+    return next(); // no args returns 200 OK
 }
