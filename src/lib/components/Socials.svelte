e5222884e90ddff53182978e09be5e4ce8efc9b4
wes <bellwitch@pm.me>
2025-12-23 13:56:16 -0500

    minor changes to socials, finished svelte5 migration

--- src/lib/components/Socials.svelte
+++ src/lib/components/Socials.svelte
@@ -4,11 +4,11 @@
 	import Link from '$lib/components/Link.svelte';
 	import CopyToClipboard from 'svelte-copy-to-clipboard';
 
-	let discordTag = '1dog#0001';
+	let discordTag = '1dog';
 	// is this correct? idk feels DRY
 	let dataTip = {
 		default: 'click to copy my username',
-		copied: 'copied 1dog#0001 to clipboard!'
+		copied: 'copied "1dog" to clipboard!'
 	};
 	let tip = $state(dataTip.default);
 
@@ -29,9 +29,10 @@
 	};
 </script>
 
+<!-- TODO: this entire section needs to be redone in a smarter way-->
 <grid class="grid grid-cols-4 gap-4 max-w-2xl place-items-center">
 	<div class="col-span-4 text-4xl font-bold text-center mt-4 mb-2">find me here:</div>
-	<div class="tooltip" data-tip={tip} value="1dog#0001">
+	<div class="tooltip" data-tip={tip} value="1dog">
 		<CopyToClipboard text={discordTag} on:copy={handleCopied} on:fail={handleFailedCopy} >
 			{#snippet children({ copy })}
 						<button onclick={copy} class="btn gap-2">
@@ -46,13 +47,21 @@
 				</CopyToClipboard>
 	</div>
 	<div>
-		<LinkedButton href="https://github.com/01dog">
+		<!-- <LinkedButton href="https://github.com/01dog">
 			<svg role="img" class="h-6 w-6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
 				><title>GitHub</title><path
 					d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
 				/></svg
 			>
 			GitHub
+		</LinkedButton> -->
+		<LinkedButton href="https://glass.photo/1dog">
+			<svg role="img" class="h-6 w-6" viewBox="0 0 640 640" xmlns="http://www.w3.org/2000/svg" 
+				><title>Glass</title><path 
+					d="M213.1 128.8L202.7 160L128 160C92.7 160 64 188.7 64 224L64 480C64 515.3 92.7 544 128 544L512 544C547.3 544 576 515.3 576 480L576 224C576 188.7 547.3 160 512 160L437.3 160L426.9 128.8C420.4 109.2 402.1 96 381.4 96L258.6 96C237.9 96 219.6 109.2 213.1 128.8zM320 256C373 256 416 299 416 352C416 405 373 448 320 448C267 448 224 405 224 352C224 299 267 256 320 256z"
+				/></svg
+			>
+			Glass
 		</LinkedButton>
 	</div>
 	<div>
